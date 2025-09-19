ed1e2baf7ab569e16f40bd61641e6af8263f1c68
awitgaming <ianezekielcapistrano42@gmail.com>
2025-09-19 18:44:39 +0800

    Step 3 & 4: Create loop

--- ThreadPriority/Form1.cs
+++ ThreadPriority/Form1.cs
@@ -19,7 +19,7 @@ namespace ThreadPriority
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            
         }
 
         private void frmTrackThread_Load(object sender, EventArgs e)
--- ThreadPriority/MyThreadClass.cs
+++ ThreadPriority/MyThreadClass.cs
@@ -2,12 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ThreadPriority
 {
     internal class MyThreadClass
     {
+        public static void Thread1()
+        {
+            for (int LoopCount = 0; LoopCount < 6; LoopCount++)
+            {
+                Thread thread = Thread.CurrentThread;
+                Console.WriteLine("Name of Thread: " + thread.Name + " = " + LoopCount);
+                Thread.Sleep(1500);
+            }
+        }//Thread1
+
+
+        public static void Thread2() { 
+            for (int LoopCount = 0; LoopCount < 6; LoopCount++)
+            {
+                Thread thread = Thread.CurrentThread;
+                Console.WriteLine("Name of Thread: " + thread.Name + " = " + LoopCount);
+                Thread.Sleep(1500); 
+            }
+
+
+        }
         
+
     }
 }
